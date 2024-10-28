495aefbb73ac6c1a34386893c2381f4ba1d16f13
LorenzSSS <stanislavlorenz25@gmail.com>
2024-10-28 17:45:08 +0100

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,452 @@
+export const data = [
+    {
+        type: 'pizza',
+        items: [
+            {
+                name: 'Чоризо фреш',
+                big: {
+                    img: '../public/items/pizzas/choriso_fresh/big_choriso_fresh.png',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                middle: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                small: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                ingredients: [
+                    {
+                        name: 'ветчина',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'сыр чеддер',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'сладкий перец',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'моцарелла',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'томатный соус',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'чеснок',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'итальянские травы',
+                        isConstant: false,
+                    },
+                ],
+            },
+            {
+                name: 'Цыпленок блю чиз',
+                big: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                middle: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                small: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                ingredients: [
+                    {
+                        name: 'ветчина',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'сыр чеддер',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'сладкий перец',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'моцарелла',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'томатный соус',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'чеснок',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'итальянские травы',
+                        isConstant: false,
+                    },
+                ],
+            },
+            {
+                name: 'Нежный лосось',
+                big: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                middle: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                small: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                ingredients: [
+                    {
+                        name: 'ветчина',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'сыр чеддер',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'сладкий перец',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'моцарелла',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'томатный соус',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'чеснок',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'итальянские травы',
+                        isConstant: false,
+                    },
+                ],
+            },
+            {
+                name: 'Сырная',
+                big: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                middle: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                small: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                ingredients: [
+                    {
+                        name: 'ветчина',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'сыр чеддер',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'сладкий перец',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'моцарелла',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'томатный соус',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'чеснок',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'итальянские травы',
+                        isConstant: false,
+                    },
+                ],
+            },
+            {
+                name: 'Пепперони фреш',
+                big: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                middle: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                small: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                ingredients: [
+                    {
+                        name: 'ветчина',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'сыр чеддер',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'сладкий перец',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'моцарелла',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'томатный соус',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'чеснок',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'итальянские травы',
+                        isConstant: false,
+                    },
+                ],
+            },
+            {
+                name: 'Четыре сыра',
+                big: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                middle: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                small: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                ingredients: [
+                    {
+                        name: 'ветчина',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'сыр чеддер',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'сладкий перец',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'моцарелла',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'томатный соус',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'чеснок',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'итальянские травы',
+                        isConstant: false,
+                    },
+                ],
+            },
+            {
+                name: 'Четыре сезона',
+                big: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                middle: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                small: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                ingredients: [
+                    {
+                        name: 'ветчина',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'сыр чеддер',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'сладкий перец',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'моцарелла',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'томатный соус',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'чеснок',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'итальянские травы',
+                        isConstant: false,
+                    },
+                ],
+            },
+            {
+                name: 'Гавайская',
+                big: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                middle: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                small: {
+                    img: '/',
+                    price: 1000,
+                    weigth: 1000,
+                },
+                ingredients: [
+                    {
+                        name: 'ветчина',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'сыр чеддер',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'сладкий перец',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'моцарелла',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'томатный соус',
+                        isConstant: true,
+                    },
+                    {
+                        name: 'чеснок',
+                        isConstant: false,
+                    },
+                    {
+                        name: 'итальянские травы',
+                        isConstant: false,
+                    },
+                ],
+            },
+        ]
+    },
+    {
+        type: 'desserts',
+        items: [
+            {
+                name: 'Рулетики с ананасами + Рулетики с брусникой, 16 шт',
+                img: '/',
+                description: 'Микс рулетиков со сгущенным молоком: 8 штук с ананасом и корицей, 8 штук с брусникой',
+                quantity: 16,
+                price: 1000,
+            },
+            {
+                name: 'Чизкейк Нью-Йорк',
+                img: '/',
+                description: 'Мы перепробовали тысячу десертов и наконец нашли любимца гостей — нежнейший творожный чизкейк',
+                quantity: 1,
+                price: 1000,
+            },
+            {
+                name: 'Сырники',
+                img: '/',
+                description: 'Любимый десерт многих наших гостей — румяные сырники из печи. Такие нежные, в меру сладкие и напоминающие детство',
+                quantity: 2,
+                price: 1000,
+            },
+            {
+                name: 'Чизкейк Банановый',
+                img: '/',
+                description: 'Солнечный снаружи и яркий по вкусу внутри. Летняя новинка — нежный чизкейк с бананом и шоколадным печеньем',
+                quantity: '1',
+                price: 1000,
+            },
+            {
+                name: 'Шоколадный кукис',
+                img: '/',
+                description: 'На вид печенье как планета, а на вкус — шоколадная комета с глазурью',
+                quantity: '1',
+                price: 1000,
+            },
+            {
+                name: 'Фондан',
+                img: '/',
+                description: 'Четверо из пяти гостей говорят «Oh la la!», когда едят этот французский десерт с хрустящей корочкой и топленой шоколадной начинкой',
+                quantity: '2',
+                price: 1000,
+            },
+            {
+                name: 'Сорбет Клубничный гранат',
+                img: '/',
+                description: 'Охлаждающий дуэт сочных ягод и спелых фруктов',
+                quantity: '1',
+                price: 1000,
+            },
+            {
+                name: 'Бруслетики',
+                img: '/',
+                description: 'Это задорные сладкие рулетики, в которых закрутился микс из натуральной брусники и фирменного пломбирного соуса',
+                quantity: '16',
+                price: 1000,
+            },
+        ]
+    },
+];
