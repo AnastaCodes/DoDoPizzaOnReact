0e271b9fd5fe15c7f574f22f251870601cca664b
Anastasiia Goncharova <anastagoncharova1995@gmail.com>
2024-10-28 18:02:00 +0100

    Logo.png and Logo.jsx changed

--- src/ui/Logo/Logo.jsx
+++ src/ui/Logo/Logo.jsx
@@ -1,5 +1,6 @@
 import s from "./Logo.module.scss";
-import img from "../../../public/favicon.png";
+import img from "../../../public/logo.png";
+import { Button } from "../Button/Button";
 
 export const Logo = () => {
   return(
@@ -8,6 +9,17 @@ export const Logo = () => {
     <p className={s.signature}>
       Додо пицца<span>Сеть пиццерий №1 в России</span>
     </p>
+
+    <div className="navigation">
+        <a href="#pizza">Пицца</a>
+        <a href="#combo">Комбо</a>
+        <a href="#snacks">Закуски</a>
+        <a href="#desserts">Десерты</a>
+        <a href="#drinks">Напитки</a>
+        <a href="#other">Другие товары</a>
+    </div>
+    <div className=""><a href="88003020060">8 800 302-00-60</a>
+    Звонок бесплатный</div>
   </div>
 
 
