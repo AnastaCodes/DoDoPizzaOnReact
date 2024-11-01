1abf2e7069b1a455fd6c9144c1259f994d584117
LorenzSSS <stanislavlorenz25@gmail.com>
2024-11-01 13:05:24 +0100

    Fixed Container

--- src/ui/ProductCard/ProductCard.jsx
+++ src/ui/ProductCard/ProductCard.jsx
@@ -1,3 +1,6 @@
 import s from './ProductCard.module.scss'
 
-export const ProductCard = () => {return <div></div>}
+export const ProductCard = () => {
+    return <div></div>}
+
+export default ProductCard
