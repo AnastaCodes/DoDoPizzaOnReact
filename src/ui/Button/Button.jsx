b39b9377c8226424069993e846391ae1123044c3
LorenzSSS <stanislavlorenz25@gmail.com>
2024-11-07 01:25:19 +0100

    Fixed data.js

--- src/ui/Button/Button.jsx
+++ src/ui/Button/Button.jsx
@@ -2,6 +2,6 @@ import s from './Button.module.scss'
 
 export const Button = ({text, type, w100}) => {
     return (
-        <button className = {`${s.Button} ${s[type, w100]}`}>{text}</button>
+        <button className = {`${s.Button} ${s[type]} ${s[w100]}`}>{text}</button>
     )
 }
