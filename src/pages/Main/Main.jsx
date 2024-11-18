20cef48d42ebea11e3b2de093f7617fb6c1aa954
Anastasiia Goncharova <anastagoncharova1995@gmail.com>
2024-11-18 19:13:42 +0100

    PizzaModal.jsx, Main.jsx, Category.jsx

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,42 +1,52 @@
-import { useNavigate } from "react-router-dom";
 import React, { useState, useRef } from "react";
 
 import { Header } from "../../sections/Header";
 import { ImageSlider } from "../../sections/ImageSlider/ImageSlider";
 import { Additional } from "../../sections/Additional/Additional";
-import { Category } from '../../sections/Category/Category'
-import { Footer } from '../../sections/Footer/Footer'
-import { PizzaModal } from '../../components/PizzaModal/PizzaModal'
-
-
-import s from "./Main.module.scss";
-
+import { Category } from "../../sections/Category/Category";
+import { Footer } from "../../sections/Footer/Footer";
+import { PizzaModal } from "../../components/PizzaModal/PizzaModal";
+import { data } from "../../data";
 
 export const Main = () => {
-  // const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState("test");
+  const [selectedItem, setSelectedItem] = useState(null);
 
-  const toggleModal = (el) => {
+  const toggleModal = (name) => {
+    const item = findItemByName(name)
     setIsModalOpen(!isModalOpen);
-    // setSelectedItem(el.target)
-    setSelectedItem("test")
-   // console.log(el)
-    console.log(selectedItem)
+    setSelectedItem(item);
+    console.log(item)
+  };
+
+  const findItemByName = (name) => {
+    for (const category of data) {
+      const item = category.items.find((item) => item.name === name);
+      if (item) return item;
+    }
+    return null;
   };
 
   return (
     <>
-    <Header/>
-    <ImageSlider/>
-    <Additional/>
-    <Category title = 'Пицца' type = 'pizza' onClick={(el)=> toggleModal(el)} />
-    <Category title = 'Комбо' type = 'combo'/>
-    <Category title = 'Закуски' type = 'appetizers'/>
-    <Category title = 'Десерты' type = 'desserts'/>
-    <Category title = 'Напитки' type = 'drinks'/>
-    <Footer />
-    <PizzaModal test={isModalOpen} onClick={()=> toggleModal()} /> 
+      <Header />
+      <ImageSlider />
+      <Additional />
+      <Category
+        title="Пицца"
+        type="pizza"
+        onClick={(name) => toggleModal(name)}
+      />
+      <Category title="Комбо" type="combo" />
+      <Category title="Закуски" type="appetizers" />
+      <Category title="Десерты" type="desserts" />
+      <Category title="Напитки" type="drinks" />
+      <Footer />
+      <PizzaModal
+        test={isModalOpen}
+        onClick={() => toggleModal(null)}
+        selectedItem={selectedItem}
+      />
     </>
   );
 };
